092f73e6a54e6e592068aa979715b00fa30f3758
e <lzq0email@gmail.com>
2019-03-25 21:13:21 +0800

    update

--- zszy/app.js
+++ zszy/app.js
@@ -1,16 +1,65 @@
-//app.js
+const WXAPI = require('wxapi/main')
 App({
+  navigateToLogin: false,
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
+    const that = this;
+    /**
+     * 初次加载判断网络情况
+     * 无网络状态下根据实际情况进行调整
+     */
+    wx.getNetworkType({
+      success(res) {
+        const networkType = res.networkType
+        if (networkType === 'none') {
+          that.globalData.isConnected = false
+          wx.showToast({
+            title: '当前无网络',
+            icon: 'loading',
+            duration: 2000
+          })
+        }
+      }
+    });
+    /**
+     * 监听网络状态变化
+     * 可根据业务需求进行调整
+     */
+    wx.onNetworkStatusChange(function (res) {
+      if (!res.isConnected) {
+        that.globalData.isConnected = false
+        wx.showToast({
+          title: '网络已断开',
+          icon: 'loading',
+          duration: 2000,
+          complete: function () {
+            that.goStartIndexPage()
+          }
+        })
+      } else {
+        that.globalData.isConnected = true
+        wx.hideToast()
+      }
+    });
+    //  获取商城名称
+    WXAPI.queryConfig({
+      key: 'mallName'
+    }).then(function (res) {
+      if (res.code == 0) {
+        wx.setStorageSync('mallName', res.data.value);
+      }
+    })
+    WXAPI.scoreRules({
+      code: 'goodReputation'
+    }).then(function (res) {
+      if (res.code == 0) {
+        that.globalData.order_reputation_score = res.data[0].score;
+      }
+    })
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        console.log('login',res);
+        console.log('login', res);
       }
     })
     // 获取用户信息
@@ -34,8 +83,40 @@ App({
         }
       }
     })
+
+    // 判断是否登录
+    let token = wx.getStorageSync('token');
+    if (!token) {
+      that.goLoginPageTimeOut()
+      return
+    }
+    WXAPI.checkToken(token).then(function (res) {
+      if (res.code != 0) {
+        wx.removeStorageSync('token')
+        that.goLoginPageTimeOut()
+      }
+    })
+  },
+  goLoginPageTimeOut: function () {
+    if (this.navigateToLogin) {
+      return
+    }
+    this.navigateToLogin = true
+    setTimeout(function () {
+      wx.redirectTo({
+        url: "/pages/authorize/authorize"
+      })
+    }, 1000)
+  },
+  goStartIndexPage: function () {
+    setTimeout(function () {
+      wx.redirectTo({
+        url: "/pages/start/start"
+      })
+    }, 1000)
   },
   globalData: {
+    isConnected: true,
     userInfo: null
   }
 })
---
+++ zszy/pages/authorize/authorize.js
@@ -0,0 +1,133 @@
+const WXAPI = require('../../wxapi/main')
+//获取应用实例
+var app = getApp();
+Page({
+  data: {
+    remind: '加载中',
+    angle: 0,
+    userInfo: {}
+  },
+  bindGetUserInfo: function (e) {
+    this.login();
+    // 回到原来的地方放
+    app.navigateToLogin = false
+    if (!e.detail.userInfo) {
+      return;
+    }
+    if (app.globalData.isConnected) {
+      wx.setStorageSync('userInfo', e.detail.userInfo)
+      // this.login();
+    } else {
+      wx.showToast({
+        title: '当前无网络',
+        icon: 'none',
+      })
+    }
+  },
+  login: function () {
+    const that = this;
+    const token = wx.getStorageSync('token');
+    if (token) {
+      WXAPI.checkToken(token).then(function (res) {
+        if (res.code != 0) {
+          wx.removeStorageSync('token')
+          that.login();
+        } else {
+          // 回到原来的地方放
+          app.navigateToLogin = false
+          wx.navigateBack();
+        }
+      })
+      return;
+    }
+    wx.login({
+      success: function (res) {
+        WXAPI.login(res.code).then(function (res) {
+          if (res.code == 10000) {
+            // 去注册
+            that.registerUser();
+            return;
+          }
+          if (res.code != 0) {
+            // 登录错误
+            wx.hideLoading();
+            // wx.showModal({
+            //   title: '提示',
+            //   content: '无法登录，请重试',
+            //   showCancel: false
+            // })
+            // return;
+          }
+          wx.setStorageSync('token', res.data.token)
+          wx.setStorageSync('uid', res.data.uid)
+          // 回到原来的地方放
+          app.navigateToLogin = false
+          wx.reLaunch({
+            url: '/pages/home/home',
+          })
+        })
+      }
+    })
+  },
+  registerUser: function () {
+    let that = this;
+    wx.login({
+      success: function (res) {
+        let code = res.code; // 微信登录接口返回的 code 参数，下面注册接口需要用到
+        wx.getUserInfo({
+          success: function (res) {
+            let iv = res.iv;
+            let encryptedData = res.encryptedData;
+            let referrer = '' // 推荐人
+            let referrer_storge = wx.getStorageSync('referrer');
+            if (referrer_storge) {
+              referrer = referrer_storge;
+            }
+            // 下面开始调用注册接口
+            WXAPI.register({
+              code: code,
+              encryptedData: encryptedData,
+              iv: iv,
+              referrer: referrer
+            }).then(function (res) {
+              wx.hideLoading();
+              that.login();
+            })
+          }
+        })
+      }
+    })
+  },
+  onLoad: function () {
+    var that = this
+  },
+  onShow: function () {
+    let that = this
+    let userInfo = wx.getStorageSync('userInfo')
+    if (!userInfo) {
+      app.goLoginPageTimeOut()
+    } else {
+      that.setData({
+        userInfo: userInfo
+      })
+    }
+  },
+  onReady: function () {
+    var that = this;
+    setTimeout(function () {
+      that.setData({
+        remind: ''
+      });
+    }, 1000);
+    wx.onAccelerometerChange(function (res) {
+      var angle = -(res.x * 30).toFixed(1);
+      if (angle > 14) { angle = 14; }
+      else if (angle < -14) { angle = -14; }
+      if (that.data.angle !== angle) {
+        that.setData({
+          angle: angle
+        });
+      }
+    });
+  }
+});
